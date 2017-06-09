a1bf598b053ff140194b9e41142b923365f4f2e5
Otavio Remedio <otavioremedio@gmail.com>
2017-06-08 21:47:57 -0300

    3. Display Messages

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,10 +3,18 @@ export class MainController {
     'ngInject';
 
       this.$http = $http;
+      this.getMessages();
   }
 
     postMessage(){
         this.$http.post('http://localhost:5000/api/message',{msg: this.message});
+    }
+
+    getMessages(){
+        var vm = this;
+        this.$http.get('http://localhost:5000/api/message').then(function(result){
+            vm.messages = result.data;
+        });
     }
 
 }
